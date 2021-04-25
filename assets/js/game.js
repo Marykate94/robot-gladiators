c2025ad223ce30c08437ee953c2beaa63b1fb3e7
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-04-25 15:55:06 -0400

    added robot name function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,23 @@
-window.alert("This is an alert!! JavaScript is running!");
+//this creates a prompt 
+
+//window.prompt("What is your robot's name");
+
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+var name = "your name";
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a messaage");
+// this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+  //fight();
